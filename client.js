217772d3f56342b50326bdb23de897b3a38426d6
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-05 22:15:04 -0500

    added comments

--- client.js
+++ client.js
@@ -1,12 +1,14 @@
 console.log('Here are all the available people:', people);
 
-$(document).ready(onReady);
+$(document).ready(onReady); //this what pulls in the jquery
 
-function onReady(){
-    let random = randomNumber(0, people.length -1);
+function onReady(){ //this was appears upon page load
+    let random = randomNumber(0, people.length -1); //defined a variable that's calling in a function and setting the parameters of randomNumber
 
     console.log('hello from jquery');
-    for(picture of people){
+    for(picture of people){ //this is a for of loop
+
+        //appending target and using string interpolation
         $('#target').append(`
             <div data-name = "${picture.name}"class="picture">
                 <img src="https://github.com/${picture.githubUsername}.png?size=250"
@@ -14,8 +16,8 @@ function onReady(){
             </div>
         `)
     }
-    $('#randomName').append(people[random].name);
-    $('.picture').on('click', function(){
+    $('#randomName').append(people[random].name);//pulling in the random images id and appending the person's random name to the DOM
+    $('.picture').on('click', function(){ //event listener on click it runs the function with a conditional
         if($(this).data('name')===people[random].name){
             alert('yay! You are correct!')
         }else{
@@ -23,8 +25,6 @@ function onReady(){
         }
     })
 }
-
-//put this inside of a function and use string interpolation?
 
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
