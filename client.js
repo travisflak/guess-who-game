ef76d9e09b44faa0b82dcdd3b0c7df1f3dfde9db
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-05 21:52:17 -0500

    base seems to be solved

--- client.js
+++ client.js
@@ -3,16 +3,25 @@ console.log('Here are all the available people:', people);
 $(document).ready(onReady);
 
 function onReady(){
+    let random = randomNumber(0, people.length -1);
+
     console.log('hello from jquery');
-    for(people of people){
+    for(picture of people){
         $('#target').append(`
-            <div>
-                <img src="https://github.com/${people.githubUsername}.png?size=250"
+            <div data-name = "${picture.name}"class="picture">
+                <img src="https://github.com/${picture.githubUsername}.png?size=250"
                 alt="Profile image of Chris"></img>
             </div>
         `)
     }
-
+    $('#randomName').append(people[random].name);
+    $('.picture').on('click', function(){
+        if($(this).data('name')===people[random].name){
+            alert('yay! You are correct!')
+        }else{
+            alert('try again!')
+        }
+    })
 }
 
 //put this inside of a function and use string interpolation?
